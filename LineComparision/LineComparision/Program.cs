e5ef528fe9e8840f2dca709524a8120879faf13c
Mabel Christina <maybelchristina@gmail.com>
2022-05-17 11:38:44 +0530

    [Mabel] Line length computation

--- LineComparision/LineComparision/Program.cs
+++ LineComparision/LineComparision/Program.cs
@@ -6,7 +6,7 @@ namespace LineComparision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello this is line program");
         }
     }
 }
